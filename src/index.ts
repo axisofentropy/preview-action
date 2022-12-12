250ea65f5812fb9ecdf4f5beae2aabd0525ee502
Adam Vollrath <adam.d.vollrath@gmail.com>
2022-12-12 16:19:59 -0600

    Barely functional Typescript action

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
+    console.log('Hello World!')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
