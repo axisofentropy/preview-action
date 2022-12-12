7602438648c49960927446dea472b9e5b53fffca
Adam Vollrath <adam.d.vollrath@gmail.com>
2022-12-12 16:39:32 -0600

    Simple test workflow and octokit types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import * as Webhooks from '@octokit/webhooks'
 
 async function run(): Promise<void> {
   try {
+    const composeFileCacheKey: string = core.getInput('compose-file-cache-key')
+    const composeFileCachePath: string = core.getInput('compose-file-cache-path')
+    const username: string = core.getInput('username')
+    const server: string = core.getInput('server')
+
+    console.log('Hello ' + username)
+
     console.log('Hello World!')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
